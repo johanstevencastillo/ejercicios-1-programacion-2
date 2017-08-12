f5828d7501fd49f95631f131eb1a3f39a7b2c118
Johan Castillo <guatequejscg@gmail.com>
2017-08-12 09:35:06 -0500

    comit inicial

---
+++ src/ejerciccio/pkg1/Ejerciccio1.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejerciccio.pkg1;
+
+/**
+ *
+ * @author Johan Castillo
+ */
+public class Ejerciccio1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("HOLA MUNDO DESDE UDEC ");
+                System.out.println("####################");
+		System.out.println("#                  #");
+		System.out.println("#                  #");
+		System.out.println("#                  #");
+		System.out.println("######       #######");
+	        System.out.println("     #       #");
+     		System.out.println("     #       #");
+		System.out.println("     #       #");
+		System.out.println("     #       #");
+		System.out.println("     #       #");
+		System.out.println("     #       #");
+		System.out.println("     #       #");
+		System.out.println("     #       #");
+		System.out.println("     #       #");
+		System.out.println("     #       #");
+		System.out.println("     #       #");
+		System.out.println("     #       #");
+                System.out.println("###############");
+                
+    }
+    
+}
